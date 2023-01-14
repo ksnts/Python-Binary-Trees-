f94ace1c7d8114262d629c2332e80374c6a4281c
John Kylle Santos <santosjohnkyllem@gmail.com>
2023-01-14 16:50:23 +0800

    defined function for searching a certain value

--- Binarytrees.py
+++ Binarytrees.py
@@ -20,3 +20,19 @@ class BinarySearchTreenode:
                 self.right.add_child(data)
             else:
                 self.right = BinarySearchTreenode(data)
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else: 
+                return False
